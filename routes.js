7faa5bcb9aa8168b9f0b7063c480f5173325ce41
isb <jaredlamont90@gmail.com>
2020-12-30 17:26:15 -0800

    major: added crawl functionality

--- routes.js
+++ routes.js
@@ -1,15 +1,30 @@
 import express from 'express';
+import validUrl from 'valid-url';
+import kickstarterCrawler from 'kickstarter-crawler';
 
 const router = express.Router();
 
 router.get('/crawl', (req, res) => {
-  res.statusCode = 200;
-  res.json({});
+  const url = req && req.query && req.query.url;
+  if (isUrl(url)) {
+    const result = kickstarterCrawler(url);
+    result.then((data) => {
+      res.statusCode = 200;
+      res.json(data);
+    })
+  } else {
+    res.statusCode = 400;
+    res.end('400 Bad Request: Malformed Url');
+  }
 });
 
 router.get('*', (req, res) => {
   res.statusCode = 400;
   res.end('400 Bad Request');
 });
+
+const isUrl = (url) => {
+  return validUrl.isHttpsUri(url) || validUrl.isHttpUri(url);
+}
 
 export default router;
